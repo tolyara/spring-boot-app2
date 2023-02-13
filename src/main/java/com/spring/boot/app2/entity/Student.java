6c35fd051f0b96609befb25a18932b201d8a1470
Anatolii <anatolii.melchenko@academysmart.com>
2023-02-14 00:28:26 +0200

    entity Student;

---
+++ src/main/java/com/spring/boot/app2/entity/Student.java
@@ -0,0 +1,59 @@
+package com.spring.boot.app2.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Long grade;
+
+    @Column
+    private Long supervisorId;
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Long grade) {
+        this.grade = grade;
+    }
+
+    public Long getSupervisorId() {
+        return supervisorId;
+    }
+
+    public void setSupervisorId(Long supervisorId) {
+        this.supervisorId = supervisorId;
+    }
+
+}
+
+
