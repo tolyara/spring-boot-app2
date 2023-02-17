8940abb80e5631dc0e7331588611195858176b2b
Anatolii <anatolii.melchenko@academysmart.com>
2023-02-17 21:01:32 +0200

    @Transactional propagation;

--- src/main/java/com/springboot/app2/service/TransactionalServiceImpl.java
+++ src/main/java/com/springboot/app2/service/TransactionalServiceImpl.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -29,10 +30,10 @@ public class TransactionalServiceImpl implements TransactionalService {
     @Override
     @Transactional
     public Student updateStudent(Student student) {
-        Student newStudent1 = new Student();
-        newStudent1 = studentRepository.save(newStudent1);
-        newStudent1.setName("stud-" + newStudent1.getId());
-        studentRepository.save(newStudent1);
+//        Student newStudent1 = new Student();
+//        newStudent1 = studentRepository.save(newStudent1);
+//        newStudent1.setName("stud-" + newStudent1.getId());
+//        studentRepository.save(newStudent1);
 
         Student existing = studentRepository.findById(student.getId()).get();
         existing.setName(student.getName());
@@ -43,7 +44,20 @@ public class TransactionalServiceImpl implements TransactionalService {
         return existing;
     }
 
-    @Transactional
+//    @Transactional // REQUIRED by default, Support a current transaction, create a new one if none exists. (Method 2 executes in existing transaction of 'parent 'method 1)
+//    @Transactional(propagation = Propagation.REQUIRES_NEW) // Create a new transaction, and suspend the current transaction if one exists.
+//    @Transactional(propagation = Propagation.NESTED) // Execute within a nested transaction if a current transaction exists, behave like REQUIRED otherwise.
+
+//    @Transactional(propagation = Propagation.MANDATORY) // Support a current transaction, throw an exception if none exists.
+    @Transactional(propagation = Propagation.NEVER) // Execute non-transactionally, throw an exception if a transaction exists.
+//    @Transactional(propagation = Propagation.NOT_SUPPORTED) // Execute non-transactionally, suspend the current transaction if one exists.
+//    @Transactional(propagation = Propagation.SUPPORTS) // Support a current transaction, execute non-transactionally if none exists.
+
+//    NOTE: Actual transaction suspension will not work out-of-the-box on all transaction managers.
+//    This in particular applies to JtaTransactionManager, which requires the jakarta.transaction.TransactionManager to be made available to it
+//    (which is server-specific in standard Jakarta EE).
+//    See Also: JtaTransactionManager.setTransactionManager(jakarta.transaction.TransactionManager)
+
     public StudentSettings updateStudentSettings(Student student) {
         StudentSettings ss = studentSettingsRepository.findAllByStudentId(student.getId());
         if (ss == null) return null;
