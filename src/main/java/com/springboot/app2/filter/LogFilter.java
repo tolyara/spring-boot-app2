2c0f91e181687b2eea80ad3fa5919a0697a168ce
Anatolii <anatolii.melchenko@academysmart.com>
2023-02-23 20:20:39 +0200

    LogFilter;

---
+++ src/main/java/com/springboot/app2/filter/LogFilter.java
@@ -0,0 +1,23 @@
+package com.springboot.app2.filter;
+
+import jakarta.servlet.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LogFilter implements Filter {
+
+    private Logger logger = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        logger.info("request.getRequestId() : " + request.getRequestId());
+        logger.info("request.getLocalAddr() : " + request.getLocalAddr());
+        chain.doFilter(request, response);
+    }
+
+}
