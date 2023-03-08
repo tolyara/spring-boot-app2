8acec0aa8ef8d6e6a64d62be79a548b95f2c2c41
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-08 17:13:08 +0200

    elasticsearch config;

---
+++ src/main/java/com/springboot/app2/config/ElasticSearchConfig.java
@@ -0,0 +1,27 @@
+package com.springboot.app2.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.erhlc.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.client.erhlc.RestClients;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+
+@Configuration
+@EnableElasticsearchRepositories("com.springboot.app2.dao.elasticsearch")
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Value("${elasticsearch.url}")
+    public String elasticSearchUrl;
+
+    @Bean
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration config = ClientConfiguration.builder()
+                .connectedTo(elasticSearchUrl).build();
+        return RestClients.create(config).rest();
+    }
+}
