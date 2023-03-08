132dff5a52c015f4aba7e1fdff9890aaaa038585
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-08 18:02:54 +0200

    person;

---
+++ src/main/java/com/springboot/app2/entity/elasticsearch/Person.java
@@ -0,0 +1,43 @@
+package com.springboot.app2.entity.elasticsearch;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+@Document(indexName = "person")
+@Setting(settingPath = "static/es-settings.json")
+public class Person {
+
+    @Id
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
