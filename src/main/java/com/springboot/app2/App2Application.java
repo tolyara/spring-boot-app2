8c50b49f1e5c3905946918051230b80c9e1cfabc
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-08 13:45:48 +0200

    EnableAsync;

--- src/main/java/com/springboot/app2/App2Application.java
+++ src/main/java/com/springboot/app2/App2Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /*
 	SystemProperties.get("java.version") -> 19.0.2
@@ -13,6 +14,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @EntityScan(basePackages = {"com.springboot.app2.entity"})
 @ServletComponentScan
+@EnableAsync // for @Async feature, some executor will be supplied by Spring if we don't create custom executor by ourselves
 public class App2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(App2Application.class, args);
---
+++ src/main/java/com/springboot/app2/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.springboot.app2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public Executor customExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("CustomExecutor-");
+        executor.initialize();
+        return executor;
+    }
+
+}
