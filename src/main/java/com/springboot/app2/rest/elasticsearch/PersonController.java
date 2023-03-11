ab3533b43f2393cfa35a96c55a89710d4a490f77
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-11 16:34:07 +0200

    comments;

--- src/main/java/com/springboot/app2/rest/elasticsearch/PersonController.java
+++ src/main/java/com/springboot/app2/rest/elasticsearch/PersonController.java
@@ -4,6 +4,12 @@ import com.springboot.app2.entity.elasticsearch.Person;
 import com.springboot.app2.service.elasticsearch.PersonService;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ *  http://localhost:9200/_cat/indices
+ *  http://localhost:9200/person?pretty=true
+ *  http://localhost:9200/person/_search?pretty=true
+ */
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
