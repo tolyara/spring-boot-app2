40bfa31c67294484e5a81df28483909a82def00a
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-13 21:17:21 +0200

    Vehicle, new fields;

--- src/main/java/com/springboot/app2/entity/elasticsearch/Vehicle.java
+++ src/main/java/com/springboot/app2/entity/elasticsearch/Vehicle.java
@@ -1,10 +1,16 @@
 package com.springboot.app2.entity.elasticsearch;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
 public class Vehicle {
 
     private String id;
-
     private String number;
+    private String name;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date created; // it will be type 'date', this type can be in different cases, like some regular format or timestamp
 
     public Vehicle() {
     }
@@ -23,6 +29,22 @@ public class Vehicle {
 
     public void setNumber(String number) {
         this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
     }
 
 }
