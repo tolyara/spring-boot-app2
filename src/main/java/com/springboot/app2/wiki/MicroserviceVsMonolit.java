7a25d178007f4883c9436bb737ac9dbbf788f314
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-16 18:23:05 +0200

    Monolit advantages:

---
+++ src/main/java/com/springboot/app2/wiki/MicroserviceVsMonolit.java
@@ -0,0 +1,31 @@
+package com.springboot.app2.wiki;
+
+/**
+ *
+ * Monolit advantages:
+ * - easy to develop;
+ * - easy to deploy;
+ * - easy to debug and support;
+ * - more productive, consumes less hardware resources if designed well;
+ * - more productive, communication with services is simple and fast, no need to use http (or other) protocols;
+ *
+ * Monolit disadvantages:
+ * - difficult to integrate new technologies, cannot easily implement new module on Python if all monolit is written on Java;
+ * - code base becomes more cumbersome and thus threshold of entering new developer to project grows up;
+ * - heavy to scale one concrete module, but it might be an advantage because in case of microservice we need to do a lot of movement/setup/changes
+ * whereas with monolith we just need to rent one more server and put the code in there;
+ *
+ *
+ * Microservice advantages:
+ *
+ * Microservice disadvantages:
+ *
+ * Optimal app development strategy:
+ * - first develop basic structure (registration, item catalog, ordering, shipping) as monolit,
+ * client's business will get time advantage on competitive market to allocate niche;
+ * - then if need to integrate new functionality (card billing), build it to separate microservice;
+ * - then if some service requires scaling, move it to separate microservice and scale (setup 2, 3 or more instances of it);
+ * - eventually, move essential features which potentially will need extension to separate microservice, bit by bit, gradually;
+ */
+public class MicroserviceVsMonolit {
+}
