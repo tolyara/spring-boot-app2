455674a53dd5891924f3263a983046ebdb6327fe
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-17 13:03:34 +0200

    Aspect;

--- src/main/java/com/springboot/app2/wiki/SpringAOP.java
+++ src/main/java/com/springboot/app2/wiki/SpringAOP.java
@@ -8,6 +8,26 @@ package com.springboot.app2.wiki;
  * AOP represents paradigm based on idea of distribution of main and support functions. Support methods are stored in ASPECTS (cross-cutting logic).
  * Examples of cross-cutting logic - logging, security check, transactions, exception handling, caching etc.
  * Spring by itself is riddled with AOP.
+ *
+ * pluses :
+ * - easy to support service logic;
+ * - business logic becomes more clear;
+ * minuses :
+ * - additional time for aspect's working;
+ *
+ *
+ *
+ * AOP proxy pattern
+ * Main class -> AOP proxy -> target method (e.g. addBook())
+ *
+ *
+ *
+ * Aspect - class responsible for end-to-end (cross) functionality
+ *
+ * Advice - method inside aspect class
+ * Advice types : before, after returning, after throwing, after, around
+ *
+ *
  */
 public class SpringAOP {
 }
