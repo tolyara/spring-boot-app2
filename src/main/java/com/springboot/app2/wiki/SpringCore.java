e81e0257522491384acbc77515c4e2db4c81b3b2
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-17 14:37:34 +0200

    SpringCore;

---
+++ src/main/java/com/springboot/app2/wiki/SpringCore.java
@@ -0,0 +1,12 @@
+package com.springboot.app2.wiki;
+
+/**
+ *
+ * Bean life cycle:
+ * app starts -> IoC container starts -> bean object create ->
+ * dependencies inject to bean -> init method -> bean is ready ->
+ * destroy-method -> app stops
+ *
+ */
+public class SpringCore {
+}
