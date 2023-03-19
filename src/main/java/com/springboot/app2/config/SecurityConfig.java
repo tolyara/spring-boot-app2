cde7519f8bb932ce700a95db32a6be361d9281c2
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-19 18:25:51 +0200

    SecurityFilterChain impl;

--- src/main/java/com/springboot/app2/config/SecurityConfig.java
+++ src/main/java/com/springboot/app2/config/SecurityConfig.java
@@ -7,14 +7,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-// TODO Replace with SecurityFilterChain, use the new requestMatchers methods https://docs.spring.io/spring-security/reference/5.8/migration/servlet/config.html
+// TODO Replace with SecurityFilterChain, use the new requestMatchers methods
+//  https://docs.spring.io/spring-security/reference/5.8/migration/servlet/config.html
+//  UPD: done
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -26,14 +30,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable() // turn off basic auth
                 .csrf().disable()   // turn off csrf protection
@@ -47,7 +51,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
+        return http.build();
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .httpBasic().disable() // turn off basic auth
+//                .csrf().disable()   // turn off csrf protection
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // don't create sessions
+//                .and()
+//                .authorizeRequests()
+////                .antMatchers(LOGIN_ENDPOINT).permitAll()
+////                .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
+//                .requestMatchers(LOGIN_ENDPOINT).permitAll()
+//                .requestMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
+//                .anyRequest().authenticated()
+//                .and()
+//                .apply(new JwtConfigurer(jwtTokenProvider));
+//    }
 
 //    @Bean
 //    public BCryptPasswordEncoder passwordEncoder() {
