d479685559117cd56c73984dcb363700655bb7df
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-20 15:09:52 +0200

    MockSpyTest;

---
+++ src/test/java/com/spring/boot/app2/fortest/MockSpyTest.java
@@ -0,0 +1,38 @@
+package com.spring.boot.app2.fortest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockSpyTest {
+
+    @Mock
+    private List<String> mockList;
+
+    @Spy
+    private List<String> spyList;
+
+    @Test
+    public void testMockList() {
+        // by default, calling the methods of mock object will do nothing
+        mockList.add("test");
+        Assert.assertNull(mockList.get(0));
+        Assert.assertNull(mockList.iterator());
+    }
+
+    @Test
+    public void testSpyList() {
+        // spy object will call the real method when not stub
+        spyList = new ArrayList<>();
+        spyList.add("test");
+        Assert.assertEquals("test", spyList.get(0));
+    }
+
+}
