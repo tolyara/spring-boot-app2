5db86dc90c3aa2f625e34f378d835a02b55364ec
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-20 16:16:27 +0200

    TestControllerIntTest;

---
+++ src/test/java/com/spring/boot/app2/rest/jwt/test/TestControllerIntTest.java
@@ -0,0 +1,33 @@
+package com.spring.boot.app2.rest.jwt.test;
+
+import com.springboot.app2.rest.test.TestController;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TestController.class)
+public class TestControllerIntTest {
+
+//    @Autowired
+    @Inject
+    private MockMvc mvc;
+
+    @Test
+    void test() throws Exception {
+        RequestBuilder request = get("/test");
+        MvcResult result = mvc.perform(request).andReturn();
+        assertEquals("", result.getResponse().getContentAsString());
+    }
+
+}
