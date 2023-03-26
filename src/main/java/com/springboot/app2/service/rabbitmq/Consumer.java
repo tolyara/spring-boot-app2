504cf549c4d3996921d3666713cd63a01a821a20
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-26 17:13:54 +0300

    rabbitmq consumer;

---
+++ src/main/java/com/springboot/app2/service/rabbitmq/Consumer.java
@@ -0,0 +1,39 @@
+package com.springboot.app2.service.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.springboot.app2.util.rabbitmq.RabbitmqUtil;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class Consumer {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @PostConstruct
+    private void init() {
+        listenForMessages();
+    }
+
+    private void listenForMessages() {
+        ConnectionFactory factory = new ConnectionFactory();
+
+        try (Connection connection = factory.newConnection()) {
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(RabbitmqUtil.QUEUE, false, false, false, null);
+            channel.queueBind(RabbitmqUtil.QUEUE, RabbitmqUtil.EXCHANGE, RabbitmqUtil.ROUTING_KEY);
+            channel.basicConsume(RabbitmqUtil.QUEUE, false,
+                    (consumerTag, message) -> { logger.info("Got a message : {} ", new String(message.getBody(), StandardCharsets.UTF_8)); },
+                    (consumerTag, sig) -> { logger.error("Got an exception : {}", sig.getMessage()); });
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+}
--- src/main/java/com/springboot/app2/util/rabbitmq/RabbitmqUtil.java
+++ src/main/java/com/springboot/app2/util/rabbitmq/RabbitmqUtil.java
@@ -5,6 +5,7 @@ public final class RabbitmqUtil {
     public static final String EXCHANGE = "our-ex";
     public static final String TYPE = "direct";
     public static final String ROUTING_KEY = "routing-key";
+    public static final String QUEUE = "our-queue";
 
     private RabbitmqUtil() {}
 
