5abaeb9978640af46fea144b23a59ca56f2c5e30
Anatolii <anatolii.melchenko@academysmart.com>
2023-04-11 23:25:32 +0300

    setup test App2ApplicationTests;

--- src/test/java/com/springboot/app2/App2ApplicationTests.java
+++ src/test/java/com/springboot/app2/App2ApplicationTests.java
@@ -2,8 +2,10 @@ package com.springboot.app2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class App2ApplicationTests {
 
 	@Test
