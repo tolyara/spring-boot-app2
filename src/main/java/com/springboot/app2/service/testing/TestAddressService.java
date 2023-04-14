dfbaca8cfa4a965591d3fa10ce4f43c0b510722a
Anatolii <anatolii.melchenko@academysmart.com>
2023-04-14 16:10:43 +0300

    TestPersonService;

---
+++ src/main/java/com/springboot/app2/service/testing/TestAddressService.java
@@ -0,0 +1,28 @@
+package com.springboot.app2.service.testing;
+
+import com.springboot.app2.dao.testing.TestAddressRepository;
+import com.springboot.app2.entity.testing.TestAddress;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestAddressService {
+
+    private final TestAddressRepository addressRepository;
+
+    @Autowired
+    public TestAddressService(TestAddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public TestAddress createAddress(String name) {
+        TestAddress newAddress = new TestAddress();
+        newAddress.setName(name);
+        return addressRepository.save(newAddress);
+    }
+
+    public TestAddress findAddress(String id) {
+        return addressRepository.findById(id).orElse(null);
+    }
+
+}
---
+++ src/main/java/com/springboot/app2/service/testing/TestPersonService.java
@@ -0,0 +1,31 @@
+package com.springboot.app2.service.testing;
+
+import com.springboot.app2.dao.testing.TestPersonRepository;
+import com.springboot.app2.entity.testing.TestAddress;
+import com.springboot.app2.entity.testing.TestPerson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestPersonService {
+
+    private final TestPersonRepository personRepository;
+    private final TestAddressService addressService;
+
+    @Autowired
+    public TestPersonService(TestPersonRepository personRepository, TestAddressService addressService) {
+        this.personRepository = personRepository;
+        this.addressService = addressService;
+    }
+
+    public TestPerson createPerson(String addressId, String personName) {
+        TestAddress address = addressService.findAddress(addressId);
+        if (address == null) return null;
+
+        TestPerson newPerson = new TestPerson();
+        newPerson.setName(personName);
+        newPerson.setAddress(address);
+        return personRepository.save(newPerson);
+    }
+
+}
