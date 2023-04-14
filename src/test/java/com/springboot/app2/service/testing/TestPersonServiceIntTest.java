2abfb18cdb77b9551e88960d93ffe87c4aa9ebae
Anatolii <anatolii.melchenko@academysmart.com>
2023-04-14 16:33:39 +0300

    createPersonTest;

--- src/test/java/com/springboot/app2/service/testing/TestPersonServiceIntTest.java
+++ src/test/java/com/springboot/app2/service/testing/TestPersonServiceIntTest.java
@@ -1,10 +1,12 @@
 package com.springboot.app2.service.testing;
 
+import com.springboot.app2.entity.testing.TestAddress;
 import com.springboot.app2.entity.testing.TestPerson;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class TestPersonServiceIntTest {
@@ -18,7 +20,18 @@ public class TestPersonServiceIntTest {
     @Test
     public void createPersonTest_noAddress() {
         TestPerson person = personService.createPerson(TestUserServiceTest.DUMMY_ID, "James Bond");
-        Assertions.assertThat(person).isNull();
+        assertThat(person).isNull();
+    }
+
+    @Test
+    public void createPersonTest() {
+        TestAddress address = addressService.createAddress("a1");
+        assertThat(address).isNotNull();
+
+        TestPerson person = personService.createPerson(address.getId(), "James Bond");
+        assertThat(person).isNotNull();
+        assertThat(person.getAddress()).isNotNull();
+        assertThat(person.getAddress().getId()).isEqualTo(address.getId());
     }
 
 }
