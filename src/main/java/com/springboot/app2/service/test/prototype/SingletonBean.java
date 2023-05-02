197c77afa5ca039247c21dcb10a297f83d9a1b4f
Anatolii <anatolii.melchenko@academysmart.com>
2023-05-02 15:42:04 +0300

    Injecting ApplicationContext;

--- src/main/java/com/springboot/app2/service/test/prototype/SingletonBean.java
+++ src/main/java/com/springboot/app2/service/test/prototype/SingletonBean.java
@@ -1,8 +1,12 @@
 package com.springboot.app2.service.test.prototype;
 
+import com.springboot.app2.util.LoggingUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 import java.time.LocalTime;
 
@@ -13,9 +17,12 @@ import java.time.LocalTime;
  * In order to describe the problem, let's configure the following beans: SingletonBean and PrototypeBean
  *
  */
-public class SingletonBean {
+//public class SingletonBean {
+public class SingletonBean implements ApplicationContextAware {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private ApplicationContext applicationContext;
 
     @Autowired
     private PrototypeBean prototypeBean;
@@ -25,8 +32,14 @@ public class SingletonBean {
     }
 
     public PrototypeBean getPrototypeBean() {
-        logger.info(String.valueOf(LocalTime.now()));
-        return prototypeBean;
+        LoggingUtil.log(logger);
+        return applicationContext.getBean(PrototypeBean.class);
+
+//        return prototypeBean;
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
