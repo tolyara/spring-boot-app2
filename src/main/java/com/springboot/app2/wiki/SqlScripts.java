04b4ce5c130465d9a90895db5b25392e2d97445a
Anatolii <anatolii.melchenko@academysmart.com>
2023-07-04 15:37:39 +0300

    SqlScripts 2;

--- src/main/java/com/springboot/app2/wiki/SqlScripts.java
+++ src/main/java/com/springboot/app2/wiki/SqlScripts.java
@@ -2,7 +2,7 @@ package com.springboot.app2.wiki;
 
 /**
  *
- * Count, Group By, Having
+ * COUNT, GROUP BY, HAVING
  *
  * select s.id, s.name, count(p.id)
  * from students s
@@ -12,7 +12,7 @@ package com.springboot.app2.wiki;
  *
  * ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
  *
- * Sub query
+ * SUB QUERY
  *
  * select distinct s.id, s.name
  * from students s
@@ -24,6 +24,23 @@ package com.springboot.app2.wiki;
  * inner join pets p on p.student_id = s.id
  * group by s.id, s.name
  * having s.id in (select s2.id from students s2 where s2.grade is not null and s2.grade > 100)
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ *
+ * NOT
+ *
+ * select distinct s.id, s.name
+ * from students s
+ * where not s.name = 'Bill'
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ *
+ * LIKE
+ *
+ * select distinct s.id, s.name
+ * from students s
+ * where s.name like '%ony%'
+ *
  *
  */
 public class SqlScripts {
