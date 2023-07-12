5caaebc544caaf035beae9718eb00b6e6153b1f1
Anatolii <anatolii.melchenko@academysmart.com>
2023-07-12 14:54:03 +0300

    SpringBoot;

---
+++ src/main/java/com/springboot/app2/wiki/SpringBoot.java
@@ -0,0 +1,28 @@
+package com.springboot.app2.wiki;
+
+/**
+ *
+ * Spring Boot is a project that provides a pre-configured set of frameworks to reduce boilerplate configuration.
+ * This way, we can have a Spring application up and running with the smallest amount of code.
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ *
+ * Features:
+ *
+ * Create stand-alone Spring applications
+ *
+ * Embed Tomcat, Jetty or Undertow directly (no need to deploy WAR files)
+ *
+ * Provide opinionated 'starter' dependencies to simplify your build configuration
+ *
+ * Automatically configure Spring and 3rd party libraries whenever possible
+ *
+ * Provide production-ready features such as metrics, health checks, and externalized configuration
+ *
+ * Absolutely no code generation and no requirement for XML configuration
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ *
+ */
+public class SpringBoot {
+}
